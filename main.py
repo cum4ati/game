eb81b17cf63f9052d0a9e9ee305f8813de92d879
itcub <asidas@MAIL.RU>
2022-04-23 18:00:57 +0400

    README.md

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ while not lose:
     user_choice = input(f'What password more popular {pass1}, {pass2}(input 1 or 2)?: ')
     while user_choice not in ('1', '2'):
         user_choice = input('Please, input 1 or 2: ')
-    if (user_choice == '1' and passwords.index(pass1) < passwords.index(pass2)) or (user_choice == '2' and  passwords.index(pass1) > passwords.index(pass2)):
+    if (user_choice == '1' and passwords.index(pass1) < passwords.index(pass2)) or (
+            user_choice == '2' and passwords.index(pass1) > passwords.index(pass2)):
         print('Congrats, you guessed correctly')
         score += 1
     else:
